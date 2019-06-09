3ebdcfc215d69bb94a0de55e388ed64a41d952ce
Mihai Adrian Pop <popmihaiadrian@gmail.com>
2019-06-09 11:24:52 +0200

    Lab 2

---
+++ Labo2/Migrations/20190511125632_TestareOn.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Labo2.Migrations
+{
+    public partial class TestareOn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_Expenses_ExpenseId",
+                table: "Comment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment");
+
+            migrationBuilder.RenameTable(
+                name: "Comment",
+                newName: "Comments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_ExpenseId",
+                table: "Comments",
+                newName: "IX_Comments_ExpenseId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Expenses_ExpenseId",
+                table: "Comments",
+                column: "ExpenseId",
+                principalTable: "Expenses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Expenses_ExpenseId",
+                table: "Comments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments");
+
+            migrationBuilder.RenameTable(
+                name: "Comments",
+                newName: "Comment");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_ExpenseId",
+                table: "Comment",
+                newName: "IX_Comment_ExpenseId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_Expenses_ExpenseId",
+                table: "Comment",
+                column: "ExpenseId",
+                principalTable: "Expenses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
